912ef87d8adaa3ff7ae3384bdd2c1b92c68e6108
Muhammad Hasnain Raza <hassnainrazam009@gmail.com>
2024-06-23 22:12:12 +0500

    completed

---
+++ app.js
@@ -0,0 +1,39 @@
+let num1 = document.getElementById('pic1');
+num1.addEventListener('mouseover',()=>{
+    num1.style.transform ='scale(1.1)';
+})
+num1.addEventListener('mouseout',()=>{
+    num1.style.transform = 'scale(1)';
+})
+
+let num2 = document.getElementById('pic2');
+num2.addEventListener('mouseover',()=>{
+    num2.style.transform ='scale(1.1)';
+})
+num2.addEventListener('mouseout',()=>{
+    num2.style.transform = 'scale(1)';
+})
+
+let num3 = document.getElementById('pic3');
+num3.addEventListener('mouseover',()=>{
+    num3.style.transform ='scale(1.1)';
+})
+num3.addEventListener('mouseout',()=>{
+    num3.style.transform = 'scale(1)';
+})
+
+let num4 = document.getElementById('pic4');
+num4.addEventListener('mouseover',()=>{
+    num4.style.transform ='scale(1.1)';
+})
+num4.addEventListener('mouseout',()=>{
+    num4.style.transform = 'scale(1)';
+})
+
+let num5 = document.getElementById('pic5');
+num5.addEventListener('mouseover',()=>{
+    num5.style.transform ='scale(1.1)';
+})
+num5.addEventListener('mouseout',()=>{
+    num5.style.transform = 'scale(1)';
+})
